d6ca03b245f72973039979fe1def281b83323f88
Arnaud BODEL <contact@osw3.net>
2020-09-28 11:23:34 +0200

    -

--- MicroTP/Personnage.php
+++ MicroTP/Personnage.php
@@ -15,4 +15,26 @@ class Personnage
      * @var integer
      */
     public $age = 53;
+
+    
+    /**
+     * Dire bonjour
+     *
+     * @param string $prenom
+     * @return string
+     */
+    public function ditBonjour(string $prenom): string
+    {
+        return "Bonjour $prenom";
+    }
+
+    /**
+     * Dire au revoir
+     *
+     * @return string
+     */
+    public function ditAurevoir(): string
+    {
+        return "Au revoir";
+    }
 }
--- MicroTP/index.php
+++ MicroTP/index.php
@@ -6,3 +6,6 @@ $personnage = new Personnage();
 
 echo "<div>Nom : ". $personnage->nom ."</div>";
 echo "<div>Age : ". $personnage->age ."</div>";
+echo "<div>Dit : ". $personnage->ditBonjour("Claire") ."</div>";
+echo "<div>Dit : ". $personnage->ditBonjour("Doug") ."</div>";
+echo "<div>Dit : ". $personnage->ditAurevoir() ."</div>";
