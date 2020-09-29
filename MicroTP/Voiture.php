dcb57488dd9afbe8c7988722e8cdf15a22364dd5
Arnaud BODEL <contact@osw3.net>
2020-09-29 11:29:42 +0200

    -

--- MicroTP/Voiture.php
+++ MicroTP/Voiture.php
@@ -5,5 +5,48 @@ class Voiture
     /**
      * nombre de roues de la voiture
      */
-    const NBRE_ROUES = 4;
+    const WHEELS = 4;
+
+    private $color;
+
+    /**
+     * Get the value of color
+     */ 
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * Set the value of color
+     *
+     * @return  self
+     */ 
+    public function setColor($color)
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    
+
+    public function start()
+    {
+    }
+
+    public function stop()
+    {
+    }
+
+    public function accelerate()
+    {
+    }
+
+    public function decelerate()
+    {
+    }
 }
+
+// $voiture = new Voiture();
+Voiture::WHEELS;
--- PorteeStatic.php
+++ PorteeStatic.php
@@ -9,7 +9,19 @@ class PorteeStatic
     // Une constate aura toujours une portée STATIC
     const MA_CONTANTE = "Ceci est une constante";
 
+    // Propriétés
     public $propriete_non_static = "Ceci est une propriété non static";
+    public static $propriete_static = "Ceci est une propriété static";
+
+    // Méthodes
+    public function methode_non_static(): string
+    {
+        return "Ceci est une methode non static";
+    }
+    public static function methode_static(): string
+    {
+        return "Ceci est une methode static";
+    }
 }
 
 
@@ -28,5 +40,21 @@ echo $instance->propriete_non_static."<br>";        // Bonne pratique ;-)
 
 echo "<hr>";
 
+// echo $instance->propriete_static."<br>";         // NON
+// echo $instance::propriete_static."<br>";         // NON
+echo $instance::$propriete_static."<br>";           // OK, mais pas joli
+echo PorteeStatic::$propriete_static."<br>";        // Bonne pratique ;-)
+
+echo "<hr>";
+
+echo $instance->methode_non_static()."<br>";        // Bonne pratique ;-)
+// echo $instance::methode_non_static()."<br>";     // NON
+// echo PorteeStatic::methode_non_static()."<br>";  // NON
+
+echo "<hr>";
+
+echo $instance->methode_static()."<br>";            // OK....
+echo $instance::methode_static()."<br>";            // OK....
+echo PorteeStatic::methode_static()."<br>";         // OK
 
 
